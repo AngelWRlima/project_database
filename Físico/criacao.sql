e6eef92503d6faa9f9353fb17f590012380c4af7
João Felipe Barbosa da Silva <104395827+JoaoFel1pe@users.noreply.github.com>
2023-09-13 08:55:06 -0300

    Update criacao.sql

--- Físico/criacao.sql
+++ Físico/criacao.sql
@@ -1,137 +1,139 @@
--- Criar tabela de País
-CREATE TABLE Pais (
-    NOME VARCHAR(255),
-    DAIMYÔ VARCHAR(255)
-    CONSTRAINT PK_PAIS PRIMARY KEY (NOME)
+-- Criar tabela de Pais
+CREATE TABLE pais (
+    nome VARCHAR(255),
+    daimyo VARCHAR(255),
+    constraint pk_pais primary key (nome)
 );
 
 -- Criar tabela da Vila
-CREATE TABLE Vila (
-    ID INT,
-    NOME VARCHAR(255),
-    SIMBOLO VARCHAR(255),
-    NOME_PAIS VARCHAR(255),
-    CONSTRAINT PK_VILA PRIMARY KEY (ID),
-    CONSTRAINT FK_VILA FOREIGN KEY (NOME_PAIS) REFERENCES Pais(NOME)
+CREATE TABLE vila (	
+    id INT,
+    nome VARCHAR(255),
+    simbolo VARCHAR(255),
+    nome_pais VARCHAR(255),
+    constraint pk_vila primary key (id),
+    constraint fk_vila foreign key (nome_pais) references pais(nome)
 );
 
 -- Criar tabela de Kage
-CREATE TABLE Kage (
-    ID_VILA INT,
-    NUMERO INT,
-    NOME VARCHAR(255),
-    CONSTRAINT PK_KAGE PRIMARY KEY (ID_Vila, Número),
-    CONSTRAINT FK_KAGE FOREIGN KEY (ID_Vila) REFERENCES Vila(ID)
+CREATE TABLE kage (
+    id_vila INT,
+    numero INT,
+    nome VARCHAR(255),
+    constraint pk_kage primary key (id_vila, numero),
+    constraint fk_kage foreign key (id_vila) references vila(id)
 );
 
--- Criar tabela de missão
-CREATE TABLE Missao (
-    ID INT,
-    Nível VARCHAR(2), --2 porque pode ser A+, S-, etc
-    Recompensa DECIMAL(10, 2), -- DECIMAL para valores monetários
-    Data DATE
-    CONSTRAINT PK_MISSAO PRIMARY KEY (ID),
+-- Criar tabela de missao
+CREATE TABLE missao (
+    id INT,
+    nivel VARCHAR(2), -- 2 porque pode ser A+, S-, etc
+    recompensa DECIMAL(10, 2), -- DECIMAL para valores monetarios
+    data DATE,
+    constraint pk_missao primary key (id)
 );
 
 -- Criar tabela Time
-CREATE TABLE Time (
-    NUMERO INT,
-    SENSEI VARCHAR(255),
-    PROTA_1 VARCHAR(255),
-    PROTA_2 VARCHAR(255),
-    PROTA_3 VARCHAR(255),
-    CONSTRAINT PK_TIME PRIMARY KEY (NUMERO)
-)
+CREATE TABLE time (
+    numero INT,
+    sensei VARCHAR(255),
+    prota_1 VARCHAR(255),
+    prota_2 VARCHAR(255),
+    prota_3 VARCHAR(255),
+    constraint pk_time primary key (numero)
+);
 
 -- Criar tabela Envia
-CREATE TABLE Envia (
-    ID_Missão INT,
-    Num_Time INT,
-    ID_Vila INT NOT NULL,
-    CONSTRAINT PK_ENVIA PRIMARY KEY (ID_Missão, Num_Time),
-    CONSTRAINT FK_ENVIA_MIS FOREIGN KEY (ID_Missão) REFERENCES Missao(ID),
-    CONSTRAINT FK_ENVIA_TIM FOREIGN KEY (Num_Time) REFERENCES Time(NUMERO),
-    CONSTRAINT FK_ENVIA_VIL FOREIGN KEY (ID_Vila) REFERENCES Vila(ID)
+CREATE TABLE envia (
+    id_missao INT,
+    num_time INT,
+    id_vila INT NOT NULL,
+    constraint pk_envia primary key (id_missao, num_time),
+    constraint fk_envia_mis foreign key (id_missao) references missao(id),
+    constraint fk_envia_tim foreign key (num_time) references time(numero),
+    constraint fk_envia_vil foreign key (id_vila) references vila(id)
 );
 
 -- Criar tabela Torneio
-CREATE TABLE Torneio (
-    ID INT PRIMARY KEY,
-    PREMIO DECIMAL(10, 2), -- DECIMAL para valores monetários
-    Data DATE
-    CONSTRAINT PK_TORNEIO PRIMARY KEY (ID)
+CREATE TABLE torneio (
+    id INT,
+    premio DECIMAL(10, 2), -- DECIMAL para valores monetarios
+    data DATE,
+    constraint pk_torneio primary key (id)
 );
 
 -- Criar tabela podios
-CREATE TABLE Podios (
-    Posição ID PRIMARY KEY
+CREATE TABLE podios (
+    posicao INT primary key
 );
 
 -- Criar tabela Participa
-CREATE TABLE Participa (
-    NUM_TIME INT,
-    ID_TORNEIO INT,
-    POSICAO INT,
-    CONSTRAINT PK_PARTICIPA PRIMARY KEY (NUM_TIME, ID_TORNEIO),
-    CONSTRAINT FK_PARTICIPA_NUM FOREIGN KEY (NUM_TIME) REFERENCES Time(Número),
-    CONSTRAINT FK_PARTICIPA_ID_TORNEIO FOREIGN KEY (ID_Torneio) REFERENCES Torneio(ID),
-    CONSTRAINT FK_PARTICIPA_POS FOREIGN KEY (POSICAO) REFERENCES Podios(POSICAO)
+CREATE TABLE participa (
+    num_time INT,
+    id_torneio INT,
+    posicao INT,
+    constraint pk_participa primary key (num_time, id_torneio),
+    constraint fk_participa_num foreign key (num_time) references time(numero),
+    constraint fk_participa_id_torneio foreign key (id_torneio) references torneio(id),
+    constraint fk_participa_pos foreign key (posicao) references podios(posicao)
 );
 
 -- Criar tabela de Ninja
-CREATE TABLE Ninja (
-    ID INT,
-    NOME VARCHAR(255),
-    RANKING INT,
-    END_CEP VARCHAR(20),
-    END_LOGRADOURO VARCHAR(100),
-    NUM_TIME, --Verificar ao inserir, modificar e deletar elemento
-    CONSTRAINT PK_NINJA PRIMARY KEY (ID),
-    CONSTRAINT FK_NINJA FOREIGN KEY (NUM_TIME) REFERENCES Time(NUMERO)
-)
+CREATE TABLE ninja (
+    id INT,
+    nome VARCHAR(255),
+    ranking INT,
+    end_cep VARCHAR(20),
+    end_logradouro VARCHAR(100),
+    num_time INT, -- Verificar ao inserir, modificar e deletar elemento
+    constraint pk_ninja primary key (id),
+    constraint fk_ninja foreign key (num_time) references time(numero)
+);
 
 -- Criar tabela Estilo Ninja
-CREATE TABLE Estilo_Ninja (
-    ID_Ninja INT,
-    Estilo VARCHAR(255),
-    CONSTRAINT PK_ESTILO PRIMARY KEY (ID_Ninja, Estilo),
-    CONSTRAINT FK_ESTILO FOREIGN KEY (ID_Ninja) REFERENCES Ninja(ID)
+CREATE TABLE estilo_ninja (
+    id_ninja INT,
+    estilo VARCHAR(255),
+    constraint pk_estilo primary key (id_ninja, estilo),
+    constraint fk_estilo foreign key (id_ninja) references ninja(id)
 );
 
 -- Criar tabela de Treina
-CREATE TABLE Treina (
-    ID_Treinador INT,
-    ID_Aluno INT,
-    CONSTRAINT PK_TREINA PRIMARY KEY (ID_Treinador, ID_Aluno)
-    CONSTRAINT FK_TREINA_TREINADOR FOREIGN KEY (ID_Treinador) REFERENCES Ninja(ID),
-    CONSTRAINT FK_TREINA_ALUNO FOREIGN KEY (ID_Aluno) REFERENCES Ninja(ID)
+CREATE TABLE treina (
+    id_treinador INT,
+    id_aluno INT,
+    constraint pk_treina primary key (id_treinador, id_aluno),
+    constraint fk_treina_treinador foreign key (id_treinador) references ninja(id),
+    constraint fk_treina_aluno foreign key (id_aluno) references ninja(id)
 );
 
 -- Criar tabela Jutsu
-CREATE TABLE Jutsu (
-    ID INT PRIMARY KEY,
-    Nome VARCHAR(255)
+CREATE TABLE jutsu (
+    id INT primary key,
+    nome VARCHAR(255)
 );
 
---Ninjutsu
-CREATE TABLE Ninjutsu(
-    ID_JUTSU INT,
-    QTD_CHACRA INT,
-    ELEMENTO VARCHAR(25),
-    CONSTRAINT PK_NINJUTSO PRIMARY KEY (ID_JUTSU)
-    CONSTRAINT FK_NINJUTSO FOREIGN KEY (ID_JUTSU) REFERENCES Jutsu(ID)
-)
---Taijutso
-CREATE TABLE Taijutso(
-    ID_JUTSU INT,
-    PODER_FISICO INT,
-    CONSTRAINT PK_TAIJUTSO PRIMARY KEY (ID_JUTSU)
-    CONSTRAINT FK_TAIJUTSO FOREIGN KEY (ID_JUTSU) REFERENCES Jutsu(ID)
-)
---Genjutsu
-CREATE TABLE Genjutsu(
-    ID_JUTSU INT,
-    PODER_ILUSAO INT,
-    CONSTRAINT PK_GENJUTSU PRIMARY KEY (ID_JUTSU)
-    CONSTRAINT FK_GENJUTSU FOREIGN KEY (ID_JUTSU) REFERENCES Jutsu(ID)
-)
+-- Ninjutsu
+CREATE TABLE ninjutsu (
+    id_jutsu INT,
+    qtd_chacra INT,
+    elemento VARCHAR(25),
+    constraint pk_ninjutsu primary key (id_jutsu),
+    constraint fk_ninjutsu foreign key (id_jutsu) references jutsu(id)
+);
+
+-- Taijutsu
+CREATE TABLE taijutsu (
+    id_jutsu INT,
+    poder_fisico INT,
+    constraint pk_taijutsu primary key (id_jutsu),
+    constraint fk_taijutsu foreign key (id_jutsu) references jutsu(id)
+);
+
+-- Genjutsu
+CREATE TABLE genjutsu (
+    id_jutsu INT,
+    poder_ilusao INT,
+    constraint pk_genjutsu primary key (id_jutsu),
+    constraint fk_genjutsu foreign key (id_jutsu) references jutsu(id)
+);
